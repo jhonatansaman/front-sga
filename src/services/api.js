abe0a5980a0eb309aa354b530267d4c062594cc7
jhonatan saman <jhonatansamann@gmail.com>
2019-07-20 22:59:29 -0300

    teste

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3001/',
-    // baseURL: 'https://back-almoxarifado.herokuapp.com',
+    // baseURL: 'http://localhost:3001',
+    baseURL: 'https://back-almoxarifado.herokuapp.com',
 })
 
 export default api;
